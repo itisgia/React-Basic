e3475368e841e6b99ebeac7644e5d2b7b9d494da
Jiah <itisgia@gmail.com>
2018-08-31 10:02:05 +1200

    adding lists

--- src/form.js
+++ src/form.js
@@ -1,12 +1,16 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 
-class Form extends Component {
+class Form extends Component{
+    constructor (props) {
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
     render(){
         return (
             <div>
-                <form>
+                <form onSubmit = {this.onSubmit}>
                     <div className="form-group">
-                        <input className="form-control" ref="text" type="text"/>
+                        <input className="form-control" ref="newItem" type="text"/>
                     </div>
                     <div className="form-group">
                         <button type="submit" className="btn btn-primary btn-lg btn-block">Add New item</button>
@@ -15,7 +19,22 @@ class Form extends Component {
             </div>
         )
     }
+
+    onSubmit(e) {
+        e.preventDefault();
+        var newItem = this.refs.newItem.value.trim();
+        if(!newItem){
+            alert('plz enter your item');
+            return;
+        }
+        this.props.addNew(newItem);
+        this.refs.newItem.value = '';
+    }
 }
 
-
-export default form;
+{/*You have to wrap a componebt in div*/}
+/*
+    You only need to export if the component is being
+    refrenced on another file
+*/
+export default Form;
--- src/index.js
+++ src/index.js
@@ -4,19 +4,92 @@ import './index.css';
 import Form from './form'
 
 class App extends Component {
+
+    constructor (props) {
+        {/*help building --*/}
+        {/*this is in app compo, bit we need to be in shopping list*/}
+        super(props);
+        {/*if somethig is chagne you should make state*/}
+        this.state = {
+            list: [
+                {
+                    id:1,
+                    item : 'Apple juice'
+                },
+                {
+                    id:2,
+                    item: 'Toilet Papers'
+                },
+                {
+                    id:3,
+                    item : 'Noodles'
+                },
+                {
+                    id:4,
+                    item: 'Oranges'
+                },
+                {
+                    id:5,
+                    item : 'Free range eggs'
+                }
+            ],
+            text : 'Hello World',
+            jumboClass : 'jumbotron text-center',
+            darkTheme : false
+        }
+
+        this.changeText = this.changeText.bind(this);
+        {/*when you want to change something you have to bind it*/}
+        this.addNewItemtoList = this.addNewItemtoList.bind(this);
+
+    }
+
+
+
     render() {
         return (
             <div>
-                <div className="jumbotron text-center">
+                <div className={this.state.jumboClass}>
                     <h1 className="display-4">Shopping List</h1>
-                    <ShoppingList/>
+                    <h3>{this.state.text}</h3>
+                    <ShoppingList list={this.state.list}/>
+                    {/*importing shoppling list and form */}
                     <br/>
                     <hr/>
-                    
-                    <Form/>
+                    <Form addNew= {this.addNewItemtoList}/>
+                    <button onClick ={this.changeText}>Change state of h3</button>
                 </div>
             </div>
         )
+    }
+
+    changeText(e){
+        e.preventDefault();
+        // alert('H3 will be changed');
+        this.setState({
+            text: 'button has been clicked',
+
+            darkTheme:!this.state.darkTheme
+        });
+
+            if (this.state.darkTheme === true) {
+                this.setState({
+                    jumboClass : 'jumbotron text-center jumboDark'
+                })
+            } else {
+                this.setState({
+                    jumboClass : 'jumbotron text-center'
+                })
+            }
+
+
+    }
+    addNewItemtoList(item){
+        var newItem = {
+            id: this.state.list.length +1,
+            item: item
+        }
+        this.setState({list:this.state.list.concat(newItem)})
     }
 }
 
@@ -26,11 +99,11 @@ class ShoppingList extends Component {
         return (
             <div>
                 <ul class="list-group">
-                    <li class="list-group-item">Cras justo odio</li>
-                    <li class="list-group-item">Dapibus ac facilisis in</li>
-                    <li class="list-group-item">Morbi leo risus</li>
-                    <li class="list-group-item">Porta ac consectetur ac</li>
-                    <li class="list-group-item">Vestibulum at eros</li>
+                    {
+                        this.props.list.map(product => {
+                            return <li key={product.id} product={product} className="list-group-item">{product.item}</li>
+                        })
+                    }
                 </ul>
             </div>
         )
