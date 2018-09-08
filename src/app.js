dc8b1849baa24ecfd8b3c98ed0bfa646cd2c8dd4
jiah kwon <itisgia@gmail.com>
2018-09-08 21:56:13 +1200

    homepage added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './app.css';
 import Form from './form';
 import Footer from './foote';
 
